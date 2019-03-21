9417b3b8766a156e2a32120da6f3cde4b9c9ed22
Geoff Robertson <geoff.robertson5@gmail.com>
2019-03-20 21:04:10 -0700

    db connected to app, most of login is done

--- Chordy/config.py
+++ Chordy/config.py
@@ -3,5 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hello-there'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:MySQL@localhost:3306/mydb'  # this is where the location of the db is passed in
     SQLALCHEMY_TRACK_MODIFICATIONS = False
